5d0a8fbc96ed40ed976956c4dbb8efa04277f30f
newaeonweb <fernando@newaeonweb.com.br>
2019-05-11 16:58:47 -0300

    added and inject band schema

--- src/modules/band/band.module.ts
+++ src/modules/band/band.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { BandController } from './band.controller';
 import { BandService } from './band.service';
-
+import { BandSchema } from './schemas/band.schema'
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: 'Band', schema: BandSchema }])],
   controllers: [BandController],
   providers: [BandService],
 })
