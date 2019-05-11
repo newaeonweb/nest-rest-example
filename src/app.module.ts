689bccedd13d1b7f88df204079fa711b7818ead8
newaeonweb <fernando@newaeonweb.com.br>
2019-05-11 16:58:15 -0300

    inject mongoose module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BandModule } from './modules/band/band.module';
 
 @Module({
-  imports: [BandModule],
+  imports: [BandModule, MongooseModule.forRoot('mongodb://localhost/nest')],
   controllers: [AppController],
   providers: [AppService],
 })
