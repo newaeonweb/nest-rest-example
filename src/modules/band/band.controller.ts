841aa3e4f5514f9e4ecab1bfe551529757d39dd3
newaeonweb <fernando@newaeonweb.com.br>
2019-05-11 16:21:07 -0300

    create band controller

---
+++ src/modules/band/band.controller.ts
@@ -0,0 +1,33 @@
+import { Controller, Get, Param, Delete, Post, Put, Body } from '@nestjs/common';
+import { Band } from './interface/band.interface';
+import { BandService } from './band.service';
+
+@Controller('band')
+export class BandController {
+  constructor(private readonly bandService: BandService) {}
+
+  @Get()
+  getAll(): Band[] {
+    return this.bandService.getAll();
+  }
+
+  @Get(':id')
+  getById(@Param('id') id: string): Band {
+    return this.bandService.getById(id);
+  }
+
+  @Delete(':id')
+  deleteById(@Param('id') id: string): Band {
+    return this.bandService.deleteById(id);
+  }
+
+  @Put(':id')
+  updateById(@Param('id') id: string, band: Band): Band {
+    return this.bandService.updateById(id, band);
+  }
+
+  @Post()
+  create(@Body() band: Band): Band {
+    return this.bandService.create(band);
+  }
+}
