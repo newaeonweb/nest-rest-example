9832865dd027f4f05341eb24ae31e066627925c9
newaeonweb <fernando@newaeonweb.com.br>
2019-05-12 11:29:01 -0300

    fixed tests

--- src/modules/band/band.controller.spec.ts
+++ src/modules/band/band.controller.spec.ts
@@ -1,18 +1,23 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { BandController } from './band.controller';
+import { BandService } from './band.service';
+import { BandModule } from './band.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 describe('Band Controller', () => {
   let controller: BandController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [BandModule, MongooseModule.forRoot('mongodb://localhost/nest')],
       controllers: [BandController],
+      providers: [BandService],
     }).compile();
 
     controller = module.get<BandController>(BandController);
   });
 
-  it('should be defined', () => {
+  fit('should be defined', () => {
     expect(controller).toBeDefined();
   });
 });
